0aeac4d357c7ec3d78885b5d81d0008425bd08b6
batuhanbaybas <71324968+batuhanbaybas@users.noreply.github.com>
2021-02-16 03:55:19 +0300

    master

--- Script/index.js
+++ Script/index.js
@@ -1,6 +1,10 @@
 let url = "https://covid19.mathdro.id/api";
 let urlCountries = `https://covid19.mathdro.id/api/countries/`;
-
+let confirmed = document.querySelector("#confirmed");
+let recovered = document.querySelector("#recovered");
+let deaths = document.querySelector("#deaths");
+let body = document.querySelector("#body");
+let country = document.querySelector("#country");
 const fetchCountry = fetch(urlCountries).then((response) => {
   return response.json();
 });
@@ -9,30 +13,22 @@ const fetchdata = fetch(url).then((response) => {
   return response.json();
 });
 fetchdata.then((data) => {
-  let confirmed = "";
-  let recovered = "";
-  let deaths = "";
-  let body = "";
-  //Cart Confirmed People Global Data
-  confirmed += `<h5 class="card-title text-center text-white">${data.confirmed.value}</h5>
+  // Card Confirmed Data
+  confirmed.innerHTML = `<h5 class="card-title text-center text-white">${data.confirmed.value}</h5>
     <h6 class="card-subtitle mb-2  text-center text-white">Global Hasta Sayısı</h6>`;
-  document.querySelector("#confirmed").innerHTML = confirmed;
-  //Cart Recovered People Global Data
-  recovered += `<h5 class="card-title text-center text-white">${data.recovered.value}</h5>
+  //Card Recovered People Global Data
+  recovered.innerHTML = `<h5 class="card-title text-center text-white">${data.recovered.value}</h5>
 <h6 class="card-subtitle mb-2 text-center text-white">Global İyileşen Sayısı</h6>`;
-  document.querySelector("#recovered").innerHTML = recovered;
-  //Cart Death People Data
-  deaths += `<h5 class="card-title text-center text-white">${data.deaths.value}</h5>
+  //Card Death People Data
+  deaths.innerHTML = `<h5 class="card-title text-center text-white">${data.deaths.value}</h5>
 <h6 class="card-subtitle mb-2 text-center text-white">Global ölüm sayısı</h6>`;
-  document.querySelector("#deaths").innerHTML = deaths;
-  //Table Global Data
-  body += `   
+//Table Global Data
+  body.innerHTML = `   
 <th scope="row">+</th>
 <td class ="bg-warning text-center">${data.confirmed.value}</td>
 <td class ="bg-success text-center">${data.recovered.value}</td>
 <td class="bg-danger text-center">${data.deaths.value}</td>
 `;
-  document.querySelector("#body").innerHTML = body;
 });
 fetchCountry.then((data) => {
   for (let i = 0; i < data.countries.length; i++) {
@@ -40,44 +36,35 @@ fetchCountry.then((data) => {
     const option = document.createElement("option");
     option.value = element.name;
     option.innerHTML = element.name;
-    document.querySelector("#country").appendChild(option);
+    country.appendChild(option);
   }
 });
 function ChangeDom(event) {
   var value = event.target.value;
-  fetch(`https://covid19.mathdro.id/api/countries/${value}`)
+  fetch(`${urlCountries}${value}`)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      let confirmed = "";
-      let recovered = "";
-      let deaths = "";
-      let body = "";
-      confirmed += `<h5 class="card-title text-center text-white">${data.confirmed.value}</h5>
+      confirmed.innerHTML = `<h5 class="card-title text-center text-white">${data.confirmed.value}</h5>
     <h6 class="card-subtitle mb-2  text-center text-white">${value} Toplam Hasta Sayısı </h6>`;
-      document.querySelector("#confirmed").innerHTML = confirmed;
-      //Cart Country Recovered People Data
-      recovered += `<h5 class="card-title text-center text-white">${data.recovered.value}</h5>
+      //Card Country Recovered People Data
+      recovered.innerHTML = `<h5 class="card-title text-center text-white">${data.recovered.value}</h5>
 <h6 class="card-subtitle mb-2 text-center text-white">${value} Toplam İyilelen Sayısı  </h6>`;
-      document.querySelector("#recovered").innerHTML = recovered;
-      //Cart Country Death People Data
-      deaths += `<h5 class="card-title text-center text-white">${data.deaths.value}</h5>
+      //Card Country Death People Data
+      deaths.innerHTML = `<h5 class="card-title text-center text-white">${data.deaths.value}</h5>
 <h6 class="card-subtitle mb-2 text-center text-white">${value} Toplam Ölüm Sayısı </h6>`;
-      document.querySelector("#deaths").innerHTML = deaths;
       //Table Country Global Data
-      body += `   
-<th scope="row">+</th>
-<td class ="bg-warning text-center">${data.confirmed.value}</td>
-<td class ="bg-success text-center">${data.recovered.value}</td>
-<td class="bg-danger text-center">${data.deaths.value}</td>
-`;
-      document.querySelector("#body").innerHTML = body;
+      body.innerHTML = `   
+      <th scope="row">+</th>
+      <td class ="bg-warning text-center">${data.confirmed.value}</td>
+      <td class ="bg-success text-center">${data.recovered.value}</td>
+      <td class="bg-danger text-center">${data.deaths.value}</td>
+      `;
     });
 }
-const selectElement = document.querySelector('#country')
-selectElement.addEventListener("change", (event)=>{
+country.addEventListener("change", (event) => {
   event.target.value;
   event.preventDefault();
-  ChangeDom(event)
-})
+  ChangeDom(event);
+});
